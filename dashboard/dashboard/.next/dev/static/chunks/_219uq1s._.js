c9c009eefeb251185af7bf286c4b14740b310d8f
Jake Fleischer <masashijf@gmail.com>
2026-09-03 12:51:43 -1000

    build errors fixed

--- dashboard/dashboard/.next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1_cxnk7._.css","static/chunks/node_modules_09t2kcp._.js","static/chunks/src_components_1upvsbf._.js","static/chunks/_0ldgc56._.js"],
-    source: "entry"
-});
